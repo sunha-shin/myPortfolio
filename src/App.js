128ac2e93e4ee4cc2596b3a20c4b8ead3ac3c40f
sunha-shin <shinsunha11@gmail.com>
2021-06-25 11:14:49 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components';
+import Header from "./components/Header";
+import About from "./sections/About";
+import Works from "./sections/Works";
+import Skills from "./sections/Skills";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Container>
+            <Header/>
+            <About/>
+            <Works/>
+            <Skills/>
+            <Footer/>
+        </Container>
+    )
 }
+
+const Container = styled.div`
+
+`;
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,13 @@
+const appData ={
+    work: {
+        title:'Recent Works',
+        description:'description1',
+        list: [
+            {
+                title:'unsplash',
+                description:'description2',
+                tags:['react', 'redux', 'redux-sata', 'styled-components']
+            }
+        ]
+    }
+}
