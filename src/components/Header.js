5b667fe7f563c7e5128a7796a654fafdddb60d06
sunha-shin <shinsunha11@gmail.com>
2021-06-25 11:14:19 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Header = () => {
+    return (
+        <Container>
+            Header
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`;
+
+export default Header;
