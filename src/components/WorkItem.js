fdd2e4232524806f506bc07b280ed431740c56b9
sunha-shin <shinsunha11@gmail.com>
2021-06-27 10:36:29 -0400

    Removed unused modules and components

--- src/components/WorkItem.js
+++ src/components/WorkItem.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import styled from 'styled-components';
-import List from "./List";
-import {appData} from "../data";
 import Tags from "./Tags";
 
 const WorkItem = ({item}) => {
